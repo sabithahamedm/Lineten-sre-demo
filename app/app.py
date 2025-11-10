0408029a647f1f5991d3f5b53be16e9184d899fd
Zabith Ahammed <sabithahamedm@gmail.com>
2025-11-10 12:29:40 +0530

    feat(app): Add minimal flast Hellow Lineten App

---
+++ app/app.py
@@ -0,0 +1,15 @@
+# app/app.py
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello Lineten!"
+
+@app.route('/health')
+def health():
+    return jsonify(status="ok"), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
